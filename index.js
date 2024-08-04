5f4f1c6430d6b0d7a2bc580f5f4e82953c37fd5e
vikasyadav01hp <vikasyadav01.hp@outlook.com>
2024-08-04 20:53:49 +0530

    second commit

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-function toggleVisibility() {
-    const userSelect = document.getElementById('userSelect');
-    const parentElements = document.querySelectorAll('.parent');
-    const childElements = document.querySelectorAll('.child');
-    
-    if (userSelect.value === 'dropdown') {
-        parentElements.forEach(parent => {
-            parent.style.display = 'block';
-        });
-        childElements.forEach(child => {
-            child.style.display = 'block';
-        });
-    } else {
-        parentElements.forEach(parent => {
-            parent.style.display = 'none';
-        });
-        childElements.forEach(child => {
-            child.style.display = 'none';
-        });
+$(document).ready(function(){
+    function childCheckByParent() {
+        $("#parent").change(function () {
+            $(this).siblings('.child').find('input[type="checkbox"]').prop('checked', $(this).is(':checked'))
+        })
     }
-}
+    childCheckByParent()
+    
+})
